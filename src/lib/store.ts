7e9a85b9b7bfd89d19e4a4b391063f18f9355c88
Alan Rejnowicz <alanrejnowicz281@gmail.com>
2025-01-11 20:13:07 +0100

    Add redux persist

--- src/lib/store.ts
+++ src/lib/store.ts
@@ -1,17 +1,38 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import { persistReducer } from "redux-persist";
+import persistStore from "redux-persist/lib/persistStore";
+import storage from "redux-persist/lib/storage";
 import cupVolumeReducer from "../slices/cup-volume";
 import progressGoalReducer from "../slices/progress-goal";
 import recordsReducer from "../slices/records";
 
+const persistConfig = {
+    key: "root",
+    storage
+};
+
+const rootReducer = combineReducers({
+    records: recordsReducer,
+    cupVolume: cupVolumeReducer,
+    progressGoal: progressGoalReducer
+});
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 const store = configureStore({
-    reducer: {
-        records: recordsReducer,
-        cupVolume: cupVolumeReducer,
-        progressGoal: progressGoalReducer
-    }
+    reducer: persistedReducer,
+    devTools: process.env.NODE_ENV !== "production",
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware({
+            serializableCheck: {
+                ignoredActions: ["persist/PERSIST", "persist/REHYDRATE"]
+            }
+        })
 });
 
 export default store;
+
+export const persistor = persistStore(store);
 
 export type RootState = ReturnType<typeof store.getState>;
 
